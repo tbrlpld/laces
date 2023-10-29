643b9d7d7a49e683057fa507c973c7212a804d03
Tibor Leupold <tibor@lpld.io>
2023-10-29 13:32:54 -0700

    Remove unnecessary app config declaration
    See also:
    https://docs.djangoproject.com/en/4.2/releases/3.2/#automatic-appconfig-discovery

--- laces/__init__.py
+++ laces/__init__.py
@@ -1,5 +1,2 @@
-default_app_config = "laces.apps.LacesAppConfig"
-
-
 VERSION = (0, 1, 0)
 __version__ = ".".join(map(str, VERSION))
--- laces/test/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = "laces.test.apps.LacesTestAppConfig"
