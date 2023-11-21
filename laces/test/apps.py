c21ac5902e541296f52f287116df3c357e90b698
Tibor Leupold <tibor@lpld.io>
2023-11-21 09:16:46 -0800

    Fix app names

--- laces/test/apps.py
+++ laces/test/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class LacesTestAppConfig(AppConfig):
     label = "laces_test"
     name = "laces.test"
-    verbose_name = "Wagtail laces tests"
+    verbose_name = "Laces test app"
