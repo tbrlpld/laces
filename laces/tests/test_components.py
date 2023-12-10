4c1bd4f65282e7de344589b76f6fc208c6228803
Tibor Leupold <tibor@lpld.io>
2023-12-10 13:08:10 -0800

    Add test for the basic Component class

---
+++ laces/tests/test_components.py
@@ -0,0 +1,30 @@
+from django.template import Context
+from django.test import SimpleTestCase
+
+from laces.components import Component
+
+
+class TestComponent(SimpleTestCase):
+    """Directly test the Component class."""
+
+    def setUp(self):
+        self.component = Component()
+
+    def test_render_html(self):
+        """Test the `render_html` method."""
+        # The default Component does not specify a `tempalte_name` attribute which is
+        # required for `render_html`.
+        with self.assertRaises(AttributeError):
+            self.component.render_html()
+
+    def test_get_context_data_parent_context_empty_context(self):
+        """
+        Test the default get_context_data.
+
+        The parent context should not matter, but we use it as it is used in
+        `render_html`.
+        """
+        result = self.component.get_context_data(parent_context=Context())
+
+        self.assertIsInstance(result, dict)
+        self.assertEqual(result, {})
