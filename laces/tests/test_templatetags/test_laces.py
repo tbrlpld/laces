d108d3131eda20e4ee8e51d7fd6ca6a0cdbf00f8
Tibor Leupold <tibor@lpld.io>
2023-12-18 20:03:19 -0800

    Test with keyword overrides with block

--- laces/tests/test_templatetags/test_laces.py
+++ laces/tests/test_templatetags/test_laces.py
@@ -115,7 +115,18 @@ class TestComponentTag(SimpleTestCase):
 
         self.assertRenderHTMLCalledWith({"test": "something else"})
 
-    # TODO: Test interaction of with block and with keyword
+    def test_with_keyword_overrides_with_block(self):
+        self.set_parent_template(
+            """
+            {% with test='something' %}
+            {% component my_component with test='something else' %}
+            {% endwith %}
+            """
+        )
+
+        self.render_parent_template_with_context({"my_component": self.component})
+
+        self.assertRenderHTMLCalledWith({"test": "something else"})
 
     def test_passing_context_to_component(self):
         class MyComponent(Component):
