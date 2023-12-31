87e44f6b14086f24bdeaae9882a6c0a40cef8495
Tibor Leupold <tibor@lpld.io>
2023-12-30 16:14:56 -0800

    Add test for kitchen sink view

---
+++ laces/test/tests/test_views.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+from django.test import RequestFactory, TestCase
+
+from laces.test.example.views import kitchen_sink
+
+
+class TestKitchenSink(TestCase):
+    def test_get(self):
+        factory = RequestFactory()
+        request = factory.get("/")
+
+        response = kitchen_sink(request)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, "<h1>Hello World</h1>")
+        self.assertContains(response, "<h1>Hello Alice</h1>")
